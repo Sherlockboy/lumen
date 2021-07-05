273c9c4fac5d992c701e2e23cb8e7d1531ad2205
Khurshidbek <kabilovhurshid5@gmail.com>
2021-07-05 12:37:28 +0500

    Log middleware added

---
+++ app/Http/Middleware/LogMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+class ExampleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::info($request);
+        
+        return $next($request);
+    }
+}
